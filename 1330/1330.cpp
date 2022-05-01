38d4626f191ad17a9cf6b088af7a6936edbbac92
InhaLSH <ss2rms@gmail.com>
2022-05-01 23:22:55 +0900

    1330번 문제 풀이 업로드

---
+++ 1330/1330.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+	int Num1;
+	int Num2;
+	std::cin >> Num1 >> Num2;
+
+	if (Num1 > Num2) {
+		std::cout << ">" << std::endl;
+	}
+	else if (Num1 < Num2) {
+		std::cout << "<" << std::endl;
+	}
+	else {
+		std::cout << "==" << std::endl;
+	}
+}
